1af060a102230337a9eae88d849834f478788780
angelo smart ide <delacruzangelo686@gmail.com>
2025-11-16 14:25:59 +0800

    lambda day two

--- lambda.java
+++ lambda.java
@@ -1,19 +1,29 @@
 import java.util.List;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.stream.Collectors;
 
 public class lambda {
 
     public static void main(String[] args) {
-        List<List<String>> listoflist = List.of(List.of("how", "nw"), List.of("yow", "saw"));
-
+        List<List<String>> listoflist = Arrays.asList(Arrays.asList("how", "nw"), Arrays.asList("yow", "saw"));
+        Set<String> intermediateResult = new HashSet<>();
         List<String> result = listoflist.stream()
-        .flatMap( z -> z.)
+        .flatMap(List::stream)
+        .filter(s -> s.startWith("n"))
+        .map(String::toUpperCase)
+        .distinct()
+        .sorted()
+        .peek(t -> intermediateResult.add(t))
+        .collect(Collectors::toList());
     }
 }
 
 //collection stream
 // = creating streams
-// concat()
-// empty()
+// concat() Stream<Integer> variable = Stream.of("A", "c")
+// empty() 
 // generate()
 // iterate()
 // of()
@@ -22,7 +32,7 @@ public class lambda {
 // map()
 // flatMap()
 // distinct()
-// sorter()
+// sorted()
 // peek()
 // limit()
 // skip()
